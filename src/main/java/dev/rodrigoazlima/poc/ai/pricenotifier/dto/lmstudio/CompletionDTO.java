e73b0e644984e549e2ccd54e8c1656fc0c862e7a
Rodrigo Lima <rodrigoazlima@gmail.com>
2025-10-24 17:45:43 -0300

    Add HelloWorldController with a greeting endpoint and corresponding tests

---
+++ src/main/java/dev/rodrigoazlima/poc/ai/pricenotifier/dto/lmstudio/CompletionDTO.java
@@ -0,0 +1,19 @@
+package dev.rodrigoazlima.poc.ai.pricenotifier.dto.lmstudio;
+
+import lombok.Builder;
+import lombok.Data;
+
+import java.util.List;
+
+
+@Data
+@Builder
+public class CompletionDTO {
+
+    private String id;
+    private String object;
+    private long created;
+    private String model;
+    private List<CompletionChoiceDTO> choices;
+
+}
